0669c9a3151c6fefa471ca818a2028d16604b342
novlan1 <1576271227@qq.com>
2024-07-05 11:26:23 +0800

    docs: 文档构建

--- script/deploy/deploy-mine.sh
+++ script/deploy/deploy-mine.sh
@@ -8,6 +8,7 @@ git init
 repoName=press-ui-v3
 userName=novlan1
 token=$1
+branch=docs
 
 git config user.email "1576271227@qq.com"
 git config user.name "novlan1"
@@ -17,6 +18,5 @@ git remote add origin https://${token}@github.com/${userName}/${repoName}
 
 git add .
 git commit -m"docs: 文档构建"
-# git checkout -b docs
-# git push origin --force
-git push origin master --force
+git checkout -b $branch
+git push origin $branch --force
